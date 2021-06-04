0222d9846edd0daecc9c240e665a5bbf1e05ca20
Meghana <meghana.madhyastha@gmail.com>
2021-06-03 20:04:54 -0400

    Add ensemble module

---
+++ Python/blockset/ensemble.py
@@ -0,0 +1,18 @@
+import pyblockset
+
+
+class BlocksetBase():
+    def __init__(self):
+        self.model = pyblockset.BlocksetBase()
+
+    def initRandomForestClassifier(self):
+        self.model.initRandomForestClassifier()
+
+    def initGradientBoostedClassifier(self):
+        self.model.initGradientBoostedClassifier()
+
+    def initRandomForestRegressor(self):
+        self.model.initRandomForestRegressor()
+
+    def initGradientBoostedRegressor(self):
+        self.model.initGradientBoostedRegressor()
