7401f37ad82cbbace011da7374d2c780c2235c04
Meghana <meghana.madhyastha@gmail.com>
2021-06-15 18:44:24 -0400

    Example run file for python blockset

--- Python/example.py
+++ Python/example.py
@@ -1,102 +1,48 @@
 import argparse, sys, time, csv, time
 import numpy as np
-import pyblockset
-from pyblockset import Config
-
-def parseCmdArgs():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--mode', action='store', dest='mode',
-                    help='<inference/pack/both>')
-
-    parser.add_argument('--layout', action='store', dest='layout', 
-                    help='<bfs/dfs/statdfs/statblockdfs/binbfs/binbfs/binstatdfs>')
-
-    parser.add_argument('--intertwine',  dest='intertwine', nargs='?', default='3',
-                    help='', const='3')
-    
-    parser.add_argument('--numbins',  dest='num_bins', nargs='?', default='1',
-                    help='', const='1')
-    
-    parser.add_argument('--numthreads', dest='num_threads', nargs='?', default='1',
-                    help='', const='1')
-
-    parser.add_argument('--numtrees', action='store', dest='num_trees',
-                    help='Number of trees')
-
-    parser.add_argument('--packfilename', action='store', dest='pack_filename', nargs='?',
-                    help='Filepath of the saved model', const='None')
-
-    parser.add_argument('--datafilename', action='store', dest='data_filename', nargs='?',
-                    const='None', help='Data filename')
-    
-    parser.add_argument('--numfiles', dest='num_files', nargs='?', default='1',
-                    const='1', help='Data filename')
-
-    parser.add_argument('--modelfilename', action='store', dest='model_filename', nargs='?',
-                    const='None.pkl', help='Filepath of the model to be read')
-    
-    parser.add_argument('--metadatafilename', action='store', dest='metadata_filename', nargs='?',
-                    const='None.pkl', help='Filepath of the model to be read')
-    
-    parser.add_argument('--algorithm', action='store', dest='algorithm', nargs='?',
-                    const='randomforest', help='randomforest/gradientboost')
-    
-    parser.add_argument('--task', action='store', dest='task', nargs='?',
-                    const='classification/regression', help='classification/regression')
-    
-    parser.add_argument('--batchsize', dest='batch_size', nargs='?', default='1',
-                    const='1', help='batch size')
-    
-    parser.add_argument('--labelcol', dest='label_col', nargs='?', default='1',
-                    const='1', help='')
-    
-    parser.add_argument('--format',  dest='format', nargs='?', default='1',
-                    const='binary', help='binary/text')
-
-    parser.add_argument('--blocksize',  dest='blocksize', nargs='?', default='128',
-                    const='128', help='Block size')
-
-    results = parser.parse_args()
-
-    return results
+import blockset
+from blockset import external, ensemble
+from sklearn import datasets
+from sklearn.ensemble import *
+from sklearn.metrics import *
+import joblib
 
 
-args = parseCmdArgs()
-Config.setConfigItem('mode', args.mode)
-Config.setConfigItem('layout', args.layout)
-Config.setConfigItem('intertwine', args.intertwine)
-Config.setConfigItem('numbins', args.num_bins)
-Config.setConfigItem('numtrees', args.num_trees)
-Config.setConfigItem('numthreads', args.num_threads)
-Config.setConfigItem('packfilename', args.pack_filename)
-Config.setConfigItem('datafilename', args.data_filename)
-Config.setConfigItem('numfiles', args.num_files)
-Config.setConfigItem('batchsize', args.batch_size)
-Config.setConfigItem('labelcol', args.label_col)
-Config.setConfigItem('modelfilename', args.model_filename)
-Config.setConfigItem('metadatafilename', args.metadata_filename)
-Config.setConfigItem('algorithm', args.algorithm)
-Config.setConfigItem('task', args.task)
-Config.setConfigItem('format', args.format)
-Config.setConfigItem('blocksize', args.blocksize)
 
-model = pyblockset.PacsetBase()
+num_trees = 100
+sklearn_save_filename = 'iris_sklearn_model.json'
+blockset_save_filename = 'iris_blockset_model.json'
 
-if Config.getValue('algorithm') == 'randomforest':
-    if Config.getValue('task') == 'classification':
-        print("rf")
-        model.initRandomForestClassifier()
-    else:
-        model.initRandomForestRegressor()
-else:
-    if Config.gettValue('task') == 'classification':
-        model.initGradientBoostedClassifier()
-    else:
-        model.initGradientBoostedRegressor()
+#Step 1: load data
+iris = datasets.load_iris()
 
-if Config.getValue('mode') == 'pack':
-    model.loadAndPack()
-else:
-    model.predict()
+#Step 2: Train a Gradient boosting / Random forest model
+sklearn_rf_model = GradientBoostingClassifier(n_estimators=num_trees)
+sklearn_rf_model.fit(iris.data, iris.target)
 
+#Step 3: Use the helper method in Blockset's external module to convert the sklearn model to json and save to file.
+external.write_to_json_gbt(sklearn_rf_model, sklearn_save_filename, regression=False)
+
+#Step 4: initialize Blockset model
+model = ensemble.BlocksetBase()
+model.initGradientBoostingClassifier()
+
+#Step 5: Load the saved sklearn json model
+model.loadJSONModel(sklearn_save_filename)
+
+#Step 6: Pack the model
+model.pack()
+
+#Step 7: Serialize the model to disk in a custom binary format
+model.serialize(blockset_save_filename)
+
+#Step 8: Load the Blockset Model
+model.loadBlocksetModel(blockset_save_filename)
+label_vec = []
+for i in iris.data:
+    #Predict labels/targets for test data
+    label_vec.append(model.predict(i))
+
+print(label_vec)
+print(sklearn_rf_model.predict(iris.data))
+print(iris.target)
